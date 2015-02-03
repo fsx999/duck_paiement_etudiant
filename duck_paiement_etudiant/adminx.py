e2c92e4e650fbfd8bc58227a9f6d2f1405aa2998
paul guichon <paul.guichon@iedparis8.net>
2015-02-03 11:33:25 +0100

    mise à jour des paiements

--- duck_paiement_etudiant/adminx.py
+++ duck_paiement_etudiant/adminx.py
@@ -7,12 +7,12 @@ from xadmin import views
 import xadmin
 
 
-class GestionFinnacierAnnee(views.Dashboard):
+class GestionFinanciereAnnee(views.Dashboard):
     base_template = 'duck_paiement_etudiant/gestion_financiere_annee.html'
     widget_customiz = False
 
     def get_context(self):
-        context = super(GestionFinnacierAnnee, self).get_context()
+        context = super(GestionFinanciereAnnee, self).get_context()
         context['years'] = AnneeUniPaiement.objects.all().order_by('-cod_anu')
         return context
     #
@@ -27,12 +27,16 @@ class GestionFinnacierAnnee(views.Dashboard):
     def get(self, request, *args, **kwargs):
         self.widgets = self.get_widgets()
         return self.template_response(self.base_template, self.get_context())
-xadmin.site.register_view(r'^gestion_financiere/$', GestionFinnacierAnnee, 'gestion_financiere_annee')
+xadmin.site.register_view(r'^gestion_financiere/$', GestionFinanciereAnnee, 'gestion_financiere_annee')
 
 
 class PaiementAdminView(object):
     pass
     # def get_urls(self, ):
 
+class PaimentAdminAnneeList(views.ListAdminView):
+    pass
+
+xadmin.site.register_modelview(r'^annee/(?P<year>\w+)/$', PaimentAdminAnneeList, name='%s_%s_test')
+
 xadmin.site.register(InsAdmEtp, PaiementAdminView)
-print xadmin.site.get_urls()
