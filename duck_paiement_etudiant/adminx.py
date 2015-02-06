15479cdee9a77111620fda7a3cfc4477b01e75a6
Alexandre Parent <barbak@gmail.com>
2015-02-06 14:15:58 +0100

    Update PaiementAdminView

--- duck_paiement_etudiant/adminx.py
+++ duck_paiement_etudiant/adminx.py
@@ -1,4 +1,5 @@
 # coding=utf-8
+from xadmin.layout import FormHelper, Layout, Fieldset, TabHolder, Container, Column, Col, Field
 from django.forms import Media
 from django.forms.models import inlineformset_factory
 from django.views.decorators.cache import never_cache
@@ -73,13 +74,35 @@ class PaiementInlineView(object):
 
 
 class PaiementAdminView(object):
-    fields = ['cod_ind']
-    readonly_fields = ['cod_ind']
+    fields = [
+        'get_nom', 'get_prenom',
+        'get_cod_etu', 'get_adresse',
+        'cod_etp', 'cod_cge',
+        'get_eta_iae', 'exoneration',
+        'demi_annee',
+        'force_encaissement']
+    readonly_fields = [
+        'get_nom', 'get_prenom',
+        'get_cod_etu', 'get_adresse',
+        'cod_etp', 'cod_cge',
+        'get_eta_iae',
+        'demi_annee']
     inlines = [PaiementInlineView]
     search_fields = ['cod_ind__cod_etu']
     hidden_menu = True
     show_bookmarks = False
     site_title = u'Dossiers financiers étudiants'
+    form_layout = Layout(Container(Col('full',
+                Fieldset(
+                    "",
+                    'get_nom', 'get_prenom',
+                    'get_cod_etu', 'get_adresse',
+                    'cod_etp', 'cod_cge',
+                    'get_eta_iae', 'exoneration',
+                    'demi_annee',
+                    'force_encaissement'
+                    , css_class="unsort no_title"), horizontal=True, span=12)
+            ))
 
     def get_media(self, *args, **kwargs):
         media = super(PaiementAdminView, self).get_media(*args, **kwargs)
@@ -87,7 +110,35 @@ class PaiementAdminView(object):
         m.add_js(['paiement_etudiant/js/paiement_etudiant.js'])
         return media+m
 
+    def get_nom(self, obj):
+        return obj.cod_ind.lib_nom_pat_ind
+    get_nom.short_description = 'Nom'
+    get_nom.allow_tags = True
 
+    def get_prenom(self, obj):
+        return '{}'.format(obj.cod_ind.lib_pr1_ind)
+    get_prenom.short_description = 'Prenom'
+    get_prenom.allow_tags = True
+
+    def get_cod_etu(self, obj):
+        return '{}'.format(obj.cod_ind.cod_etu)
+    get_cod_etu.short_description = 'Code étudiant'
+    get_cod_etu.allow_tags = True
+
+    def get_cod_opi(self, obj):
+        return '{}'.format(obj.cod_ind.cod_ind_opi)
+    get_cod_opi.short_description = 'Code opi'
+    get_cod_opi.allow_tags = True
+
+    def get_adresse(self, obj):
+        return '{}'.format(obj.cod_ind.get_full_adresse(obj.cod_anu.cod_anu))
+    get_adresse.short_description = 'Adresse'
+    get_adresse.allow_tags = True
+
+    def get_eta_iae(self, obj):
+        return '{}'.format(obj.annulation())
+    get_eta_iae.short_description = 'Etat de l\'inscription administrative'
+    get_eta_iae.allow_tags = True
 
 class PaimentAdminAnneeList(views.ListAdminView):
 
