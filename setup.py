197118af8caebc5422ba896be5755d0d16a7e36b
paul guichon <paul.guichon@iedparis8.net>
2016-03-01 15:57:52 +0100

    migration duck

--- setup.py
+++
@@ -1,4 +0,0 @@
-from distutils.core import setup
-
-setup(name='duck_paiement_etudiant', version='', packages=['duck_paiement_etudiant'], url='', license='',
-      author='paul guichon', author_email='paul.guichon@iedparis8.net', description='')
